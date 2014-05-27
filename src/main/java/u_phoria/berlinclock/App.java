07b92df99e4f10786291d6041de45a8dcc59fe09
Uros Rapajic <uros.rapajic@gmail.com>
2014-05-27 22:11:01 +0100

    Initial

---
+++ src/main/java/u_phoria/berlinclock/App.java
@@ -0,0 +1,13 @@
+package u_phoria.berlinclock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
